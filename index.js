4af5dfeb185938febe2512703baec244d8bded19
Tony Moraes <tonylds1@gmail.com>
2019-06-24 22:18:50 -0300

    Postal Calculator

--- index.js
+++ index.js
@@ -7,4 +7,32 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/postage', (req, res) => {
+    const weight = +req.query.weight
+    const type = req.query.type
+    const obj = { weight: weight, type: type, result: calculateRate(weight, type) }
+
+    res.render('pages/postage', obj)
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  function calculateRate(weight, type) {
+    let baseValue = 0;
+    const rate = 0.15;
+      switch(type) {
+        case 'Letters (Stamped)':
+          baseValue = 0.55;
+          break
+        case 'Letters (Metered)':
+            baseValue = 0.50;
+          break
+        case 'Large Envelopes (Flats)':
+            baseValue = 1.00;
+          break
+        case 'First-Class Package Service—Retail':
+            baseValue = 0.35;
+          break
+      }
+
+      return (baseValue + (Math.ceil(weight) - 1) * 0.15).toFixed(2);
+  }
